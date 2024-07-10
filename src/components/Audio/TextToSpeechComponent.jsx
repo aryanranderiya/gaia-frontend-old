0af8781fe239eecd460e3c1cba4a2faf52800508
Aryan <aryan.k.randeriya@gmail.com>
2024-07-11 01:53:36 +0530

    Remove responsive voice script

--- src/components/Audio/TextToSpeechComponent.jsx
+++ src/components/Audio/TextToSpeechComponent.jsx
@@ -1,47 +1,60 @@
-import { VoiceIcon, VolumeHighIcon } from "../icons";
+import { VoiceIcon, VolumeHighIcon, PlayIcon } from "../icons";
 import * as React from "react";
 import { Button } from "@nextui-org/button";
+import { Spinner } from "@nextui-org/spinner";
 
 export default function TextToSpeech({ text }) {
-  const [isPlaying, setIsPlaying] = React.useState(false);
+  // const [loading, setLoading] = React.useState(false);
+  // const [isPaused, setIsPaused] = React.useState(false);
+  // const [isPlaying, setIsPlaying] = React.useState(false);
+  // const [icon, setIcon] = React.useState(<VoiceIcon />);
 
-  const synth = window.speechSynthesis;
-  const utterance = new SpeechSynthesisUtterance(text);
-  utterance.pitch = 1.1;
-  utterance.rate = 1.1;
+  // const togglePlayback = () => {
+  //   if (responsiveVoice.isPlaying()) {
+  //     if (isPaused) {
+  //       console.log("test2");
+  //       responsiveVoice.resume();
+  //       setIsPlaying(true);
+  //       setIsPaused(false);
+  //     } else {
+  //       console.log("test1");
+  //       responsiveVoice.pause();
+  //       setIsPaused(true);
+  //       setIsPlaying(false);
+  //     }
+  //   } else {
+  //     console.log("test3");
+  //     setLoading(true);
+  //     responsiveVoice.speak(text, "UK English Female", {
+  //       onstart: () => {
+  //         setLoading(false);
+  //         setIsPlaying(true);
+  //       },
+  //       onend: () => {
+  //         setIsPlaying(false);
+  //       },
+  //     });
+  //   }
+  // };
 
-  for (const voice of synth.getVoices())
-    if (voice.name === "Google UK English Female") utterance.voice = voice;
-
-  utterance.addEventListener("end", () => {
-    synth.cancel();
-    setIsPlaying(false);
-  });
-
-  const playAudio = () => {
-    synth.cancel();
-    synth.speak(utterance);
-    setIsPlaying(true);
-  };
-  const togglePlayback = () => {
-    if (synth.speaking) {
-      synth.cancel();
-      setIsPlaying(false);
-    } else {
-      playAudio();
-    }
-  };
+  // React.useEffect(() => {
+  //   if (loading) setIcon(<Spinner size="sm" color="primary" />);
+  //   else if (isPlaying) setIcon(<VolumeHighIcon color="foreground" />);
+  //   else if (isPaused) setIcon(<PlayIcon />);
+  //   else setIcon(<VoiceIcon />);
+  // }, [loading, isPaused, isPlaying]);
 
   return (
     <Button
-      onPress={togglePlayback}
-      variant={isPlaying ? "shadow" : "light"}
+      // onPress={togglePlayback}
+      // variant={isPlaying ? "shadow" : "light"}
       size="sm"
       className="w-fit p-0 h-fit rounded-md"
       style={{ minWidth: "22px" }}
       color="primary"
+      isIconOnly
     >
-      {isPlaying ? <VolumeHighIcon color="foreground" /> : <VoiceIcon />}
+      {/* {icon} */}
     </Button>
   );
 }
