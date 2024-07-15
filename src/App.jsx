b4fae1d3e14e62085ac1f635f606032fe893ac84
Aryan <aryan.k.randeriya@gmail.com>
2024-07-15 18:06:40 +0530

    Setup unauthorised dialog when not logged in

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,8 @@ import * as React from "react";
 import MainInterface from "./pages/MainInterface";
 import Landing from "./layouts/Landing";
 import { Route, Routes } from "react-router-dom";
-import { apiauth } from "./apiaxios";
 
 function App() {
-  React.useEffect(() => {
-    apiauth
-      .get("http://127.0.0.1:8000/refreshToken")
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
-
   return (
     <>
       <Routes>
--- src/pages/MainInterface.jsx
+++ src/pages/MainInterface.jsx
@@ -12,6 +12,15 @@ import WebsiteName from "../components/TopWebsiteName";
 import Sidebar from "../layouts/Sidebar";
 import MainChat from "./MainChat";
 import Explore from "./Explore";
+import { apiauth } from "../apiaxios";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from "../components/Shadcn/Dialog";
+import { Button } from "@nextui-org/button";
 
 export default function MainInterface() {
   const location = useLocation();
@@ -19,6 +28,7 @@ export default function MainInterface() {
   const sidebarRef = React.useRef(null);
   const contentContainerRef = React.useRef(null);
   const [isSidebarVisible, setSidebarVisible] = React.useState(true);
+  const [open, setOpen] = React.useState(false);
   const isMobileScreen = useMediaQuery("(max-width: 600px)");
 
   React.useEffect(() => {
@@ -51,32 +61,86 @@ export default function MainInterface() {
 
   React.useEffect(() => {
     if (isMobileScreen && isSidebarVisible) toggleSidebar();
+
+    apiauth
+      .post("http://127.0.0.1:8000/refreshToken")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        const status = error?.response?.status;
+        if (status === 401 || status > 400) setOpen(true);
+        console.error("Error fetching data:", error);
+      });
   }, []);
 
   return (
-    <div className="main_container">
-      <Sidebar sidebarref={sidebarRef} toggleSidebar={toggleSidebar} />
-
-      <div
-        ref={contentContainerRef}
-        onClick={hideSidebar}
-        className="main_chat"
+    <>
+      <Dialog
+        open={open}
+        onOpenChange={(e) => {
+          if (!e) navigate("/login");
+        }}
+        className="rounded-full"
       >
-        <div className="chat_sidebar_toggle_btn">
-          <CloseOpenSidebarBtn toggleSidebar={toggleSidebar} />
+        <DialogContent className="bg-zinc-900 text-white border-none flex flex-col gap-3 md:rounded-2xl rounded-2xl max-w-[350px]">
+          <DialogHeader>
+            <DialogTitle className="text-center text-2xl  select-text">
+              Log in
+            </DialogTitle>
+            <DialogDescription className="text-center select-text text-sm">
+              You have been Signed out.
+              <br />
+              Please login again to use GAIA!
+            </DialogDescription>
+          </DialogHeader>
+
+          <div className="flex justify-center mt-2 gap-2">
+            <Button
+              size="md"
+              variant="flat"
+              color="primary"
+              onClick={() => navigate("/login")}
+            >
+              Login
+            </Button>
+
+            <Button
+              size="md"
+              variant="flat"
+              color="primary"
+              onClick={() => navigate("/signup")}
+            >
+              Signup
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+
+      <div className="main_container">
+        <Sidebar sidebarref={sidebarRef} toggleSidebar={toggleSidebar} />
+
+        <div
+          ref={contentContainerRef}
+          onClick={hideSidebar}
+          className="main_chat"
+        >
+          <div className="chat_sidebar_toggle_btn">
+            <CloseOpenSidebarBtn toggleSidebar={toggleSidebar} />
+          </div>
+          <WebsiteName />
+
+          <Routes>
+            <Route
+              path="chat"
+              element={<MainChat toggleSidebar={toggleSidebar} />}
+            />
+
+            <Route path="explore" element={<Explore />} />
+            <Route path="*" element={<Navigate to="/404" />} />
+          </Routes>
         </div>
-        <WebsiteName />
-
-        <Routes>
-          <Route
-            path="chat"
-            element={<MainChat toggleSidebar={toggleSidebar} />}
-          />
-
-          <Route path="explore" element={<Explore />} />
-          <Route path="*" element={<Navigate to="/404" />} />
-        </Routes>
       </div>
-    </div>
+    </>
   );
 }
